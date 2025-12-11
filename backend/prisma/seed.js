bd18c5cc1c88f7f8ebc001adef3b1ae5eea0fddc
Peteka <grapaju@gmail.com>
2025-12-11 15:24:02 -0300

    fix: Convert seed.js to ESM

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -3,8 +3,8 @@
 // Execute: npm run db:seed
 // =============================================================================
 
-const { PrismaClient } = require('@prisma/client');
-const bcrypt = require('bcryptjs');
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient();
 
