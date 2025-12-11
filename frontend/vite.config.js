774f924e75648d8fe510e382218c6b17ed2d5815
Peteka <grapaju@gmail.com>
2025-12-11 13:55:31 -0300

    config: Increase chunk size warning limit to 1MB

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1000, // 1MB limit
+  },
   server: {
     port: 5173,
     proxy: {
